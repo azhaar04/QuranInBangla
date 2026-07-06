372af485ac0880b6bc715e5e1a936a435cd937e6
Azharul Islam <azharul.xyz@gmail.com>
2026-07-07 00:57:05 +0600

    build ruku api's

--- backend/apps/quran/urls.py
+++ backend/apps/quran/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from apps.quran.views import SurahDetailView, SurahListView
+from apps.quran.views import (
+    RukuDetailView,
+    RukuListView,
+    SurahDetailView,
+    SurahListView,
+    SurahRukuListView,
+)
 
 app_name = 'quran'
 
 urlpatterns = [
     path('surahs/', SurahListView.as_view(), name='surah_list'),
-    path('surahs/<int:number>/', SurahDetailView.as_view(), name='surah_detail'),
+    path('surahs/<int:surah_number>/', SurahDetailView.as_view(), name='surah_detail'),
+    path('surahs/<int:surah_number>/rukus/', SurahRukuListView.as_view(), name='surah_ruku_list'),
+    path('rukus/', RukuListView.as_view(), name='ruku_list'),
+    path('rukus/<int:ruku_number>/', RukuDetailView.as_view(), name='ruku_detail'),
 ]
--- backend/apps/quran/views.py
+++ backend/apps/quran/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from apps.quran.models import Surah
-from apps.quran.serializers import SurahSerializer
+from apps.quran.models import Ruku, Surah
+from apps.quran.serializers import RukuSerializer, SurahSerializer
 
 
 class SurahListView(generics.ListAPIView):
@@ -15,5 +15,26 @@ class SurahDetailView(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = SurahSerializer
     lookup_field = 'number'
+    lookup_url_kwarg = 'surah_number'
     queryset = Surah.objects.all()
-    
+
+
+class RukuListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Ruku.objects.all()
+    serializer_class = RukuSerializer
+
+
+class RukuDetailView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = RukuSerializer
+    lookup_field = 'ruku_number'
+    queryset = Ruku.objects.all()
+
+
+class SurahRukuListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = RukuSerializer
+
+    def get_queryset(self):
+        return Ruku.objects.filter(surah__number=self.kwargs['surah_number'])
